29f454eeac0b40a6b5675d54224520a421bd3861
Calvin D <cdevogel25@gmail.com>
2017-11-25 14:42:01 -0600

    cleanup before working on db

--- main.py
+++ main.py
@@ -18,35 +18,3 @@ class Cli:
                 self.exchange.price(cmd[1:])
             elif cmd[0] == CMD_LIST[-1]:
                 self.c_exit = True
-
-
-
-# BASE_URL = 'https://api.cryptowat.ch/markets/'
-
-
-
-# def main():
-#     c_exit = False
-#     while not c_exit:
-#         cmd_s = input('cryptic ~ ')
-#         cmd = cmd_s.split(' ')
-#         if cmd[0] == CMD_LIST[0]:
-#             if len(cmd) > 1:
-#                 c_help(cmd[1:])
-#             else:
-#                 c_help()
-#         elif cmd[0] == CMD_LIST[1]:
-#             if len(cmd) < 4:
-#                 c_help([cmd[0]])
-#                 continue
-#             price(cmd[1:])
-#         elif cmd[0] == 'clear':
-#             os.system('clear')
-#         elif cmd[0] == CMD_LIST[3]:
-#             show()
-#         elif cmd[0] == CMD_LIST[-1]:
-#             c_exit = True
-#         else:
-#             print('invalid command...')
-
-# main()
