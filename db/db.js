4bfc09ded93174e01ed533c16f1ab8555d239453
Calvin D <cdevogel25@gmail.com>
2017-11-25 19:47:18 -0600

    javascript is real now

--- db/db.js
+++ db/db.js
@@ -11,14 +11,17 @@ var config = {
 
 firebase.initializeApp(config)
 
-var db = firebase.database()
+let db = {
+  db: firebase.database(),
+  read_db: (path) => {
+    this.db.ref(path).on('value', (data) => {
+      return data
+    })
+  },
+  write_db: (path, payload) => {
+    // will overwrite any data at this location!!
+    this.db.ref(path).set(payload)
+  }
+}
 
-var ref = db.ref()
-ref.once('value')
-	.then(function(snapshot) {
-		var name = snapshot.child("name").val()
-	})
-
-setTimeout(function() {
-	console.log(ref.name)
-}, 3000)
+export {db}
