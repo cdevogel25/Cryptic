8b2344ead16f5462c841a1ab07ee7de242088521
Calvin D <cdevogel25@gmail.com>
2017-11-28 09:34:49 -0600

    experiments continue with promising results

--- db/db.js
+++ db/db.js
@@ -1,80 +1,53 @@
 import {request} from 'https'
 import firebase, { database } from 'firebase'
-import dataFrame from '../api_ref/example.json'
-
-var config = {
-	apiKey: "AIzaSyBOO6VOLFovome29KGVwpP1r1HijNbYbX4",
-  authDomain: "crypt-ic.firebaseapp.com",
-  databaseURL: "https://crypt-ic.firebaseio.com",
-  projectId: "crypt-ic",
-  storageBucket: "crypt-ic.appspot.com",
-  messagingSenderId: "59440889225"
-}
-
-firebase.initializeApp(config)
-let db = firebase.database()
+import urls from '../lib/urls.json'
+import config from '../lib/config.json'
 
 class DBException {
-  constructor(message) {
-    this.message = message
-    this.name = 'dbException'
-  }
-  toString() {
-    return this.name + ': "' + this.message + '"'
-  }
+    constructor (message) {
+        this.message = message
+        this.name = 'DBException'
+    }
+    toString() {
+        return this.name + ': "' + this.message + '"'
+    }
 }
 
 class Database {
-  constructor() {
-    // does nothing
-  }
-  readDb(path) {
-    db.ref(path).on('value', (snapshot) => {
-      console.log(snapshot.val())
-    })
-  }
-  overWriteDB(path, payload) {
-    db.ref(path).set(payload)
-  }
-  writeDB(path, payload) {
-    db.ref(path).push().set(payload)
-  }
-  update(data) {
-    return data
-  }
+    constructor(urls, config) {
+        firebase.initializeApp(config)
+        this.urls = urls
+        this.db = firebase.database()
+    }
+    async readDBsync(path) {
+        var res = await new Promise((resolve, reject) => {
+            this.db.ref(path).on('value', (snapshot) => {
+                resolve(snapshot.val())
+            })
+        })
+        return res
+    }
+    writeDB(path, payload, overwrite) {
+        if(overwrite) {
+            this.db.ref(path).set(payload)
+        } else {
+            this.db.ref(path).push().set(payload)
+        }
+    }
+    apiRequest(reqHost, reqPath, destPath, overwrite) {
+        request({
+            host: reqHost,
+            path: reqPath
+        }, (res) => {
+            var dataStr = ''
+            res.on('data', (data) => {
+                dataStr += data
+            })
+            res.on('end', () => {
+                this.db.ref(destPath, JSON.parse(dataStr), overwrite)
+            })
+        })
+    }
 }
 
-let newDB = new Database()
-
-// request({
-//   host: "api.cryptowat.ch",
-//   path: '/markets/gdax/ethusd/summary'
-// }, (res) => {
-//   var dataStr = ''
-//   res.on('data', (data) => {
-//     dataStr += data
-//     console.log(dataStr)
-//   })
-//   res.on('end', () => {
-//     newDB.writeDB(undefined, JSON.parse(dataStr))
-//     console.log('done!')
-//   })
-// }).end()
-
-function apiRequest(reqHost, reqPath) {
-  request({
-    host: reqHost,
-    path: reqPath
-  }, (res) => {
-    var dataStr = ''
-    res.on('data', (data) => {
-      dataStr += data
-    })
-    res.on('end', () => {
-      newDB.writeDB(undefined, JSON.parse(dataStr))
-      console.log('done!')
-    })
-  }).end()
-}
-
-newDB.readDb()
+let testDB = new Database(urls, config)
