6529a26c69c46fedf5325a8b4eeaa30437171d0d
Calvin D <cdevogel25@gmail.com>
2017-11-28 12:56:49 -0600

    promises are very cool

--- db/db.js
+++ db/db.js
@@ -1,31 +1,35 @@
-import {request} from 'https'
+import { request } from 'https'
 import firebase, { database } from 'firebase'
 import urls from '../lib/urls.json'
 import config from '../lib/config.json'
-
-class DBException {
-    constructor (message) {
-        this.message = message
-        this.name = 'DBException'
-    }
-    toString() {
-        return this.name + ': "' + this.message + '"'
-    }
-}
+import { DBException } from '../lib/exceptions.js'
 
 class Database {
-    constructor(urls, config) {
+    constructor(urls, config, minAllowance) {
         firebase.initializeApp(config)
         this.urls = urls
         this.db = firebase.database()
+        this.minAllowance = minAllowance
     }
-    async readDBsync(path) {
-        var res = await new Promise((resolve, reject) => {
+    async checkAllowance() {
+        var response = await this.readDB('/allowance/remaining').catch((error) => {
+            console.log(error)
+            return null
+        })
+        if(response != null) {
+            return response
+        }
+    }
+    readDB(path) {
+        return new Promise((resolve, reject) => {
             this.db.ref(path).on('value', (snapshot) => {
-                resolve(snapshot.val())
+                if(snapshot.val() === null) {
+                    reject(new DBException('received null data'))
+                } else {
+                    resolve(snapshot.val())
+                }
             })
         })
-        return res
     }
     writeDB(path, payload, overwrite) {
         if(overwrite) {
@@ -34,20 +38,48 @@ class Database {
             this.db.ref(path).push().set(payload)
         }
     }
+    /* TODO: allow specification of location within db for each request
+             maybe set so each reqPath is the same as the DB path?
+    */
     apiRequest(reqHost, reqPath, destPath, overwrite) {
-        request({
-            host: reqHost,
-            path: reqPath
-        }, (res) => {
-            var dataStr = ''
-            res.on('data', (data) => {
-                dataStr += data
-            })
-            res.on('end', () => {
-                this.db.ref(destPath, JSON.parse(dataStr), overwrite)
-            })
+        this.readDB('allowance/remaining').then((allowance) => {
+            if(allowance <= this.minAllowance) {
+                throw new DBException('allowance too low')
+            } else {
+                request({
+                    host: reqHost,
+                    path: reqPath
+                }, (res) => {
+                    var dataStr = ''
+                    res.on('data', (data) => {
+                        dataStr += data
+                    })
+                    res.on('end', () => {
+                        var payload = JSON.parse(dataStr)
+                        payload.result.timestamp = Date.now()
+                        this.writeDB(reqPath, payload.result, overwrite)
+                        this.writeDB('/allowance', payload.allowance, true)
+                    })
+                }).end()
+            }
+        }).catch((error) => {
+            console.log(error.toString())
         })
+    }
+    async debugRead(path) {
+        var response = await this.readDB(path).catch((error) => {
+            console.log(error)
+            return null
+        })
+        if(response != null) {
+            console.log(response)
+        }
     }
 }
 
-let testDB = new Database(urls, config)
+const DB = new Database(urls, config, 2000000)
+try {
+    DB.apiRequest(DB.urls.api, DB.urls.price.gdax.ethusd, '/', false)
+} catch (DBException) {
+    console.log(DBException.toString())
+}
