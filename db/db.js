c1f8782d85225087eae4633d9c42e626295d69ca
Calvin D <cdevogel25@gmail.com>
2017-11-29 15:10:34 -0600

    rewriting for gdax api

--- db/db.js
+++ db/db.js
@@ -1,26 +1,29 @@
+import { PublicClient } from 'gdax'
 import { request } from 'https'
 import firebase, { database } from 'firebase'
 import urls from '../lib/urls.json'
 import config from '../lib/config.json'
 import { DBException } from '../lib/exceptions.js'
 
+/*
+TODO: decide what data needs to stay and when
+overwrite rules:
+    price: false,
+    orderbook: true
+    orders: true
+    summary: true
+*/
+
+/* TODO: rewrite Database using gdax-node library
+    currently: building for public api, eventually for private api
+*/
+
 class Database {
-    constructor(urls, config, minAllowance) {
+    constructor(config, type) {
         firebase.initializeApp(config)
-        this.urls = urls
+        this.client = new PublicClient(type)
         this.db = firebase.database()
-        this.minAllowance = minAllowance
-        this.noUpdate = false
         this.initTime = Date.now()
-    }
-    async checkAllowance() {
-        var response = await this.readDB('/allowance/remaining').catch((error) => {
-            console.log(error)
-            return null
-        })
-        if(response != null) {
-            return response
-        }
     }
     readDB(path) {
         return new Promise((resolve, reject) => {
@@ -40,62 +43,11 @@ class Database {
             this.db.ref(path).push().set(payload)
         }
     }
-    /* TODO: allow specification of location within db for each request
-             maybe set so each reqPath is the same as the DB path?
-    */
-    apiRequest(reqHost, reqPath, overwrite) {
-        this.readDB('allowance/remaining').then((allowance) => {
-            // if noUpdate, check time and set to false or whatever
-            if(this.noUpdate) {
-                if((Date.now() - this.initTime) >= 3600000) {
-                    this.noUpdate = false
-                    this.initTime = Date.now()
-                }
-            }
-            // if allowance remaining is less than minAllowance, set noUpdate to true and continue
-            if((allowance <= this.minAllowance)) {
-                this.noUpdate = true
-                throw new DBException('allowance too low')
-            } else if(!this.noUpdate) {
-                request({
-                    host: reqHost,
-                    path: reqPath
-                }, (res) => {
-                    var dataStr = ''
-                    res.on('data', (data) => {
-                        dataStr += data
-                    })
-                    res.on('end', () => {
-                        var payload = JSON.parse(dataStr)
-                        payload.result.timestamp = Date.now()
-                        this.writeDB(reqPath, payload.result, overwrite)
-                        this.writeDB('/allowance', payload.allowance, true)
-                    })
-                }).end()
-            }
-        }).catch((error) => {
-            console.log(error.toString())
+    async apiRequest(overwrite) {
+        this.client.getProductTicker().then((data) => {
+            console.log(data)
         })
-    }
-    async debugRead(path) {
-        var response = await this.readDB(path).catch((error) => {
-            console.log(error)
-            return null
-        })
-        if(response != null) {
-            console.log(response)
-        }
     }
 }
 
-/*
-TODO: decide what data needs to stay and when
-overwrite rules:
-    price: false,
-    orderbook: true
-    orders: true
-    summary: true
-*/
-
-const DB = new Database(urls, config, 2000000)
-export { DB }
+const DB = new Database(config.firebase, config.types.eth.usd)
