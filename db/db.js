40cddd513a881f0b9aacb072aa0d41e77004a752
Calvin <cdevogel25@gmail.com>
2017-12-04 18:32:28 -0600

    moved stuff to db

--- db/db.js
+++ db/db.js
@@ -4,6 +4,9 @@ import firebase, { database } from 'firebase'
 import urls from '../lib/urls.json'
 import config from '../lib/config.json'
 import { DBException } from '../lib/exceptions.js'
+import 'ws'
+import {clearInterval} from 'timers'
+import {EventEmitter} from 'events'
 
 /*
 TODO: decide what data needs to stay and when
@@ -17,6 +20,64 @@ overwrite rules:
 /* TODO: rewrite Database using gdax-node library
     currently: building for public api, eventually for private api
 */
+
+class Listener extends EventEmitter {
+    constructor(productIDs) {
+        super()
+        this.productIDs = productIDs
+        this.websocketURI = urls.ws-uri
+        this.connect()
+    }
+    connect() {
+        if(this.socket) {
+            clearInterval(this.pinger)
+            this.socket.close()
+        }
+        this.socket = new WebsocketClient(this.websocketURI)
+        this.socket.on('message', this.onMessage.bind(this))
+        this.socket.on('open', this.onOpen.bind(this))
+        this.socket.on('close', this.onClose.bind(this))
+        this.socket.on('error', this.onError.bind(this))
+    }
+    disconnect() {
+        clearInterval(this.pinger)
+
+        if(!this.socket) {
+            throw new Error('could not disconnect, not connected')
+        }
+        this.socket.close()
+        this.socket = null
+    }
+    onOpen() {
+        this.emit('open')
+        this.socket.send(JSON.stringify({
+            type: 'subscribe',
+            product_ids: this.productIDs
+        }))
+        this.pinger = setInterval(() => {
+            if(this.socket) {
+                this.socket.ping('keepalive')
+            }
+        }, 30000)
+    }
+    onClose() {
+        clearInterval(this.pinger)
+        this.socket = null
+        this.emit('close')
+    }
+    onMessage(data) {
+        this.emit('message', JSON.parse(data))
+    }
+    onError(err) {
+        if(!err) {
+            return
+        }
+        if(err.message === 'unexpected server response (429)') {
+            throw new Error('too fast. throttling')
+        }
+        this.emit('error', err)
+    }
+}
 
 class Database {
     constructor(config) {
